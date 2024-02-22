c176ddf749b879c3ee4d78d45a7e3470074cf665
xpertweb <appweb.live@gmail.com>
2024-02-22 21:27:41 +0530

    useRef

--- src/app/components/Form.js
+++ src/app/components/Form.js
@@ -1,10 +1,11 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState,useRef, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 export default function LoginForm() {
     let [name, setName] = useState('');
+    const nameRef = useRef(null);
     let [nameErr, setNameErr] = useState('');
     let [email, setEmail] = useState('');
     let [emailErr, setEmailErr] = useState('');
@@ -12,6 +13,7 @@ export default function LoginForm() {
     let [passwordErr, setPasswordErr] = useState('');
     let [confirmPassword, setConfirmPassword] = useState('');
     let [confirmPasswordErr, setConfirmPasswordErr] = useState('');
+
 
     const handleNameInputChange = (e) => {
         const nameRegex = /^[0-9A-Za-z]{6,16}$/;
@@ -21,7 +23,7 @@ export default function LoginForm() {
         }else{
             setNameErr('name should be between 6 and 16 characters, alphanumeric only')
         }
-        setName(e.target.value)
+        nameRef.current.value = e.target.value 
     }
     const handleEmailInputChange = (e) => {
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -59,6 +61,7 @@ export default function LoginForm() {
     
             console.log({
                 name, 
+                nameRef,
                 password,
                 confirmPassword, 
                 email
@@ -81,7 +84,7 @@ export default function LoginForm() {
                 <Form onSubmit={handleSubmit} className='col-6 bg-dark text-white border-3 rounded p-4'>
                     <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Name </Form.Label>
-                        <Form.Control onChange={handleNameInputChange} value={name} type="text" placeholder="Enter name" />
+                        <Form.Control onChange={handleNameInputChange} ref={nameRef} type="text" placeholder="Enter name" />
                         {/* err message  */}
                         <Form.Text className="text-danger">
                             {nameErr !== '' && nameErr}
